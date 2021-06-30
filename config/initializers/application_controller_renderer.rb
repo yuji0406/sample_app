2f6f0ec7234628ee103b7b049570cabf0b574070
EC2 Default User <ec2-user@ip-172-31-8-13.us-east-2.compute.internal>
2021-06-30 12:54:45 +0000

    [Finish]2-10

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'e36a4c7b03bce122162ed12cddad3504aa92d606cec2acd7b15283442f9db5807904adf8929cea58c582c8dd4942de8c11b874e7901dbaba8d067e32670147af'
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  get 'todolists/new'
+  get "/" => "homes#top",as:'top'
+  post 'todolists' => 'todolists#create'
+  get 'todolists' => 'todolists#index'
+  get 'todolists/:id' => 'todolists#show',as:'todolist'
+  get 'todolists/:id/edit' => 'todolists#edit',as:'edit_todolist'
+  patch 'todolists/:id' => 'todolists#update',as:'update_todolist'
+  delete 'todolists/:id' => 'todolists#destroy',as:'destroy_todolist'
+
+end
